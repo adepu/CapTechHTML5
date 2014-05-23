f9e9fada7edf822b8446412130ad449d179a8f20
Daniel Golman <dgolman@vt.edu>
2014-05-23 08:42:57 -0400

    single article collection

---
+++ _assets/backbone/collections/singleArticle.js
@@ -0,0 +1,6 @@
+var Single_Article = Backbone.Collection.extend({
+	model: Article,
+	initialize: function(models, options) {
+	    this.url = 'http://html5news.herokuapp.com/category/' + options.id;
+	}
+});
